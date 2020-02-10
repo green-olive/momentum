cc1c6689cf2864a8b7ca82e23c5c8ae5618ce293
green-olive <chuu1992@gmail.com>
2020-02-10 21:26:31 +0900

    환영 문구 수정

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,43 @@
 const container = document.querySelector(".container"),
-    title = container.querySelector("h1");
+    title = container.querySelector("#clock-text"),
+    dateText = document.querySelector("#date-text"),
+    dayText = document.querySelector("#day-text");
 
 
 function whatTimeIs() {
     const now = new Date();
+    const years = now.getFullYear();
+    const months = now.getMonth();
+    const dates = now.getDate();
+    const days = now.getDay();
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
 
+
+    dateText.innerText = `${years}년 ${months < 10 ? `0${months}` : `${months}`}월 ${dates < 10 ? `0${dates}` : `${dates}`}일`
+    switch (days) {
+        case 1:
+            dayText.innerText = `월요일`;
+            break;
+        case 2:
+            dayText.innerText = `화요일`;
+            break;
+        case 3:
+            dayText.innerText = `수요일`;
+            break;
+        case 4:
+            dayText.innerText = `목요일`;
+            break;
+        case 5:
+            dayText.innerText = `금요일`;
+            break;
+        case 6:
+            dayText.innerText = `토요일`;
+            break;
+        default:
+            dayText.innerText = `일요일`;
+    }
     title.innerText = `${hours < 10 ? `0${hours}` : `${hours}`} : ${minutes < 10 ? `0${minutes}` : `${minutes}`} : ${seconds < 10 ? `0${seconds}` : `${seconds}`}`;
 
 }
--- js/greetings.js
+++ js/greetings.js
@@ -5,6 +5,16 @@ const form = document.querySelector(".js-form"),
 const YOUR_NAME = "currentName",
     SHOWING = "showing";
 
+const greetingText = ['행복한 하루 되세요', '오늘도 화이팅', '다 잘 될 거에요', '즐거운 하루 되세요', '당신은 최고에요', '아주 잘 하고 있어요', '기분 좋은 하루 되세요'];
+
+const TEXT_NUMBER = greetingText.length;
+
+
+function genRandom() {
+    const number = Math.floor(Math.random() * greetingText.length);
+    return number;
+}
+
 // 이름 저장하는 함수
 function saveName(text) {
     localStorage.setItem(YOUR_NAME, text);
@@ -25,18 +35,19 @@ function askForName() {
     form.addEventListener("submit", handleSubmit);
 }
 
-function paintingGreeting(text) {
+function paintingGreeting(text, num) {
     form.classList.remove(SHOWING);
     greeting.classList.add(SHOWING);
-    greeting.innerText = `Have a Nice Day, ${text} :) !`
+    greeting.innerText = `${greetingText[num]}, ${text} :) !`
 }
 
 function loadName() {
     const currentName = localStorage.getItem(YOUR_NAME);
+    const randomNumber = genRandom();
     if (currentName === null) {
         askForName();
     } else {
-        paintingGreeting(currentName);
+        paintingGreeting(currentName, randomNumber);
     }
 }
 
